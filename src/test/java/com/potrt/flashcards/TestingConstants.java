6940cc4329d03a0de0d73e91c16a3873fbfa3326
sonicbigboom <sbb-github@potrt.com>
2023-08-07 01:53:08 -0400

    Added final modifier to constants.

--- src/test/java/com/potrt/flashcards/TestingConstants.java
+++ src/test/java/com/potrt/flashcards/TestingConstants.java
@@ -1,15 +1,15 @@
 package com.potrt.flashcards;
 
 public interface TestingConstants {
-    public static String wordIKanji = "私";
-    public static String wordIFurigana = "わたし";
-    public static String wordIDefinition = "I";
-    public static String phraseOnePersonKanji = "一人";
-    public static String phraseOnePersonFurigana = "ひとり";
-    public static String phraseOnePersonDefinition = "one person";
-    public static String verbToDrinkKanjiBase = "飲";
-    public static String verbToDrinkFuriganaBase = "の";
-    public static String verbToDrinkEnding = "む";
-    public static String verbToDrinkDefinition = "to drink";
-    public static JapaneseVerbForm verbFormDictionary = new JapaneseVerbForm(true, true, JapaneseVerbForm.Form.PRESENT_INDICATIVE);
+    public static final String wordIKanji = "私";
+    public static final String wordIFurigana = "わたし";
+    public static final String wordIDefinition = "I";
+    public static final String phraseOnePersonKanji = "一人";
+    public static final String phraseOnePersonFurigana = "ひとり";
+    public static final String phraseOnePersonDefinition = "one person";
+    public static final String verbToDrinkKanjiBase = "飲";
+    public static final String verbToDrinkFuriganaBase = "の";
+    public static final String verbToDrinkEnding = "む";
+    public static final String verbToDrinkDefinition = "to drink";
+    public static final JapaneseVerbForm verbFormDictionary = new JapaneseVerbForm(true, true, JapaneseVerbForm.Form.PRESENT_INDICATIVE);
 }
